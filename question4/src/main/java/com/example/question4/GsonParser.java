facbe789f59864e8173928aa8f78e2d22725d843
h86656519 <h86656519@gmail.com>
2019-09-29 01:51:59 +0800

    新增註解

--- question4/src/main/java/com/example/question4/GsonParser.java
+++ question4/src/main/java/com/example/question4/GsonParser.java
@@ -12,7 +12,7 @@ public class GsonParser {
 //        ArrayList<Repo> person = gson.fromJson(jsonString, new TypeToken<ArrayList<Repo>>(){}.getType());
         Repo temp = gson.fromJson(jsonString, new TypeToken<Repo>(){}.getType()); //要抓得資料就只有一個array
         ArrayList<Repo> person = new ArrayList<Repo>();
-        person.add(temp);
+        person.add(temp); //將資料中的 "每一組資料(Repo)" 放到ArrayList 裡
         return person;
     }
 }
